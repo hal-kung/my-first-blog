8337bf51699300c9ced240c5edd6163c1fbbccf3
hal-kung <hal.kung.chan@gmail.com>
2019-10-30 00:51:28 +0900

    Apply Comment

--- blog/models.py
+++ blog/models.py
@@ -20,3 +20,17 @@ class Post(models.Model):
     def get_markdown_text_as_html(self):
         """MarkDown記法で書かれたtextをHTML形式に変換して返す"""
         return markdown(self.text)
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
